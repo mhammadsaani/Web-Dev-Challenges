e5e61c3c529690ae950d68b5fa0341ab09c7b66f
Muhammad Hammad Sani <m.hammadsaani@gmail.com>
2022-05-14 17:59:22 +0500

    Solo Project 3 added

---
+++ Module-3/solo-project-3/password-generator/index.js
@@ -0,0 +1,37 @@
+let btnTopLeft = document.getElementById("btn-top-left-el")
+let btnTopRight = document.getElementById("btn-top-right-el")
+let btnBottomLeft = document.getElementById("btn-bottom-left-el")
+let btnBottomRight = document.getElementById("btn-bottom-right-el")
+
+
+let characters = [" ", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ":", ";", "<", "="]
+
+function userInput(){
+    return document.getElementById("userInput").value
+}
+
+
+
+function getRandomPassword(){
+    let password = ""
+    if (userInput()){
+        for (let i = 0; i < userInput(); i++){
+            password += characters[Math.floor(Math.random() * characters.length)]
+        }
+        return password
+    }else{
+        for (let i = 0; i < 15; i++){
+            password += characters[Math.floor(Math.random() * characters.length)]
+        }
+        return password
+    }
+}
+
+function generatePasswords(){
+    btnTopLeft.textContent = getRandomPassword()
+    btnBottomLeft.textContent = getRandomPassword()
+    btnTopRight.textContent = getRandomPassword()
+    btnBottomRight.textContent =  getRandomPassword()
+}
+
+generatePasswords()
